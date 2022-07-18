572278579e14a5b1292b90f4a40cce7fdc029ece
Carlos Jaime <jaime_andrek@hotmail.com>
2022-07-18 10:23:53 -0300

    first commit

---
+++ app/login/criar/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../assets/css/login.css">
+    <link rel="stylesheet" href="../../../assets/css/reset.css">
+    <link rel="stylesheet" href="../../../assets/lib/fontawesome-free-6.1.1-web/css/all.css">
+    <title>Wjcrypto Login</title>
+</head>
+<body>
+    <div class="create-container">
+        <div class="create-banner">
+            <img src="../../../assets/files/banner.svg">
+        </div>
+        <div class="create-content">
+            <div class="create">
+                <img src="../../../assets/files/logo.png">
+                <div class="input">
+                    <i class="fa-solid fa-user"></i>
+                    <input placeholder="Insira seu e-mail" required name="email" type="email">
+                </div>
+                <div class="input">
+                    <i class="fa-solid fa-user"></i>
+                    <input placeholder="Insira seu e-mail" required name="email" type="email">
+                </div>
+                <div class="input">
+                    <i class="fa-solid fa-user"></i>
+                    <input placeholder="Insira seu e-mail" required name="email" type="email">
+                </div>
+                <div class="input">
+                    <i class="fa-solid fa-user"></i>
+                    <input placeholder="Insira seu e-mail" required name="email" type="email">
+                </div>
+                <div class="input">
+                    <i class="fa-solid fa-user"></i>
+                    <input placeholder="Insira seu e-mail" required name="email" type="email">
+                </div>
+                <button>Cadastrar</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ app/login/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/css/login.css">
+    <link rel="stylesheet" href="../../assets/css/reset.css">
+    <link rel="stylesheet" href="../../assets/lib/fontawesome-free-6.1.1-web/css/all.css">
+    <title>Wjcrypto Login</title>
+</head>
+
+<body>
+    <form>
+        <div class="login-container">
+            <div class="login-content">
+                <img src="../../assets/files/logo.png">
+
+                <div class="input">
+                    <i class="fa-solid fa-user"></i>
+                    <input placeholder="Insira seu e-mail" required name="email" type="email">
+                </div>
+                <div class="input">
+                    <i class="fa-solid fa-key"></i>
+                    <input placeholder="Insira seu e-mail" required name="email" type="password">
+                </div>
+                <button>Entrar</button>
+                <p>Não possui conta? <a href="criar">Clique aqui</a>
+            </div>
+        </div>
+    </form>
+</body>
+
+</html>
