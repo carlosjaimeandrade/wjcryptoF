b64b20a5cec75f9f72ee1b1bd916287146fb2d71
Carlos Jaime <jaime_andrek@hotmail.com>
2022-07-18 21:27:11 -0300

    criando dashboard

---
+++ app/dashboard/index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/css/reset.css">
+    <link rel="stylesheet" href="../../assets/css/dashboard.css">
+    <link rel="stylesheet" href="../../assets/css/navbar.css">
+    <link rel="stylesheet" href="../../assets/css/footer.css">
+    <link rel="stylesheet" href="../../assets/lib/fontawesome-free-6.1.1-web/css/all.css">
+    <title>Wjcrypto Login</title>
+</head>
+
+<body>
+    <div class="navbar">
+        <img src="../../assets/files/logo.png">
+        <div class="navbar-action">
+            <p><i class="fa-solid fa-angle-down"></i> Bem vindo Carlos</p>
+            <div class="drop-box">
+                <button><i class="fa-solid fa-person-walking-arrow-right"></i> Sair</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="dashboard-container">
+        <div class="dashboard-content">
+            <div class="content-operations">
+                <div class="value-content">
+                    <div class="value-info">
+                        <p>Dinheiro disponível</p>
+                        <p>Conta: 554751</p>
+                    </div>
+                    <div class="value">
+                        R$ 428 <span class="rest">53</span>
+                    </div>
+                </div>
+                <div class="operations">
+                    <button>Depositar</button>
+                    <button class="mr-2">Retirar</button>
+                    <button>Transferir</button>
+                </div>
+            </div>
+            <div class="content-history">
+                <div class="history-title">
+                    <div>Histórico de entrada</div>
+                </div>
+                <div class="history">
+                    <p>Descrição da opeção</p>
+                    <p>20/05/2022 10:24:05</p>
+                </div>
+                <div class="history">
+                    <p>Descrição da opeção</p>
+                    <p>20/05/2022 10:24:05</p>
+                </div>
+            </div>
+        </div>
+        <div class="dashboard-content">
+            <div class="content-history">
+                <div class="history-title">
+                    <div>Histórico de saída</div>
+                </div>
+                <div class="history">
+                    <p>Descrição da opeção</p>
+                    <p>20/05/2022 10:24:05</p>
+                </div>
+                <div class="history">
+                    <p>Descrição da opeção</p>
+                    <p>20/05/2022 10:24:05</p>
+                </div>
+            </div>
+            <div class="content-history">
+                <div class="history-title">
+                    <div>Histórico de transferencia</div>
+                </div>
+                <div class="history">
+                    <p>Descrição da opeção</p>
+                    <p>20/05/2022 10:24:05</p>
+                </div>
+                <div class="history">
+                    <p>Descrição da opeção</p>
+                    <p>20/05/2022 10:24:05</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="footer">
+        Desenvolvido por Carlos Jaime
+    </footer>
+</body>
+
+</html>
